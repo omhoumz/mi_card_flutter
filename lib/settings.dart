e375caf3aaa977e0ae14fb9c8c672c4e0e8b0b34
Omar Houmz <omar.houmz@adevinta.com>
2019-07-23 16:01:37 +0100

    chore: add seetings

---
+++ lib/settings.dart
@@ -0,0 +1,6 @@
+const appName = 'Mi Card';
+
+const fullName = 'Omar Houmz';
+const myFunction = 'FRONT END ENGINEER';
+const phoneNumber = '+212 123 456 789';
+const email = 'contact@omarhoumz.com';
